70b000ad6b794e6f014ba5241ffe2f56404d7062
massoni007 <128920928+massoni007@users.noreply.github.com>
2026-03-25 19:35:37 -0500

    Update start_portal.bat with relative path for portability

--- pgina personal/portal-maestria/start_portal.bat
+++ pgina personal/portal-maestria/start_portal.bat
@@ -6,5 +6,5 @@ echo.
 echo Presiona CTRL + C en esta ventana para detener el servidor.
 echo.
 start http://localhost:8000
-d:\antigravity\pagina_maestria\php\php.exe -S localhost:8000
+..\releases\v1.0.0\pagina_maestria\php\php.exe -S localhost:8000
 pause
